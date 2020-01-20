620051fe19e16a1c013f49d1b62fc9879328abfb
Boris3138 <58107184+Boris3138@users.noreply.github.com>
2020-01-20 12:12:32 +0300

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+import pygame
+import random
+
+WIDTH = 700
+HEIGHT = 800
+FPS = 60
+
+
+WHITE = (255, 255, 0)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+BROWN = (160, 100, 80)
+GREEN = (102, 255, 0)
+BROWN1 = (101, 67, 33)
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Burger")
+clock = pygame.time.Clock()
+
+class Toast(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((100, 20))
+        self.image.fill(BROWN)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT - 10
+        self.speedx = 0
+
+    def update(self):
+        self.speedx = 0
+        keystate = pygame.key.get_pressed()
+        if keystate[pygame.K_LEFT]:
+            self.speedx = -8
+        if keystate[pygame.K_RIGHT]:
+            self.speedx = 8
+        self.rect.x += self.speedx
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+
+class Mob(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((70, 20))
+        self.image.fill(random.choice([WHITE, GREEN, BROWN1, RED]))
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(WIDTH - self.rect.width)
+        self.rect.y = random.randrange(-100, -40)
+        self.speedy = random.randrange(3, 6)
+        self.speedx = random.randrange(0, 1)
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+        
+        
+        if self.rect.top > HEIGHT + 10 or self.rect.left < -25 or self.rect.right > WIDTH + 20:
+            self.rect.x = random.randrange(WIDTH - self.rect.width)
+            self.rect.y = random.randrange(-100, -50)
+            self.speedy = random.randrange(3, 6)
+            
+mobs = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+player = Toast()
+all_sprites.add(player)
+for i in range(7):
+    m = Mob()
+    all_sprites.add(m)
+    mobs.add(m)
+
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    all_sprites.update()
+    
+    screen.fill(BLACK)
+    all_sprites.draw(screen)
+    pygame.display.flip()
+
+pygame.quit()
